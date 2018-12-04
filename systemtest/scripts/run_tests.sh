4ef25a52298c123bd96fd4e78d25d845c0eb6fec
imohamed <imohamed@avaya.com>
2018-12-04 02:57:40 -0700

    Keeping only topic-operator and its dependencies.

--- systemtest/scripts/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-TESTCASE=$1
-
-if [ -n "$TESTCASE" ]; then
-    EXTRA_ARGS="-Dit.test=$TESTCASE"
-fi
-
-mvn -e -q verify -pl systemtest -Djava.net.preferIPv4Stack=true -DtrimStackTrace=false -DjunitTags=acceptance $EXTRA_ARGS
